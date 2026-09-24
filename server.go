a34e0a48d3221a0809fd258de1a04913babb5ad8
James Togher <jamestogher567@gmail.com>
2026-09-24 14:29:24 -0400

    parallelized with goroutines

--- server.go
+++ server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"io"
+	"log"
 	"net/http"
-	"github.com/Jtog123/OmniGlobe/internal/worldbank"
+	"sync"
+
 	"github.com/Jtog123/OmniGlobe/internal/country"
+	"github.com/Jtog123/OmniGlobe/internal/worldbank"
 )
 
 // World Bank request string example
@@ -43,10 +45,12 @@ func main() {
 
 //Assemble a list of all countries IS03's
 
-var addCountry = "FRA"
-var year = "2025"
-var requestString = fmt.Sprintf("https://api.worldbank.org/v2/country/%s/indicator/NY.GDP.MKTP.CD?format=json&date=%s", addCountry, year)
+//var addCountry = "FRA"
+//var year = "2025"
+//var requestString = fmt.Sprintf("https://api.worldbank.org/v2/country/%s/indicator/NY.GDP.MKTP.CD?format=json&date=%s", addCountry, year)
+//var countriesMap
 
+/*
 func makeRequest() {
 	fmt.Println("making request!!")
 
@@ -99,38 +103,123 @@ func makeRequest() {
 	fmt.Println("data:", data[0])
 }
 
+*/
 
 
-//retrieve all ISO3 idents for now
-func getISO3Identifier() {
-	resp, err := http.Get("http://api.worldbank.org/v2/country?format=json&per_page=500")
-	if err != nil {
-		log.Fatalf("Failed to send request: %v", err)
+
+
+var countriesMap = make(map[string]string)
+
+
+func requestCountryByISO3Parallel(iso3Code string) (country.Country, error) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	var infoData []country.CountryResponse
+	var gdpData []country.CountryGDPResponse
+	var err1, err2 error
+
+	//Fetch metadata concurrently
+	go func() {
+		defer wg.Done()
+		infoFormatString := fmt.Sprintf("http://api.worldbank.org/v2/country/%s/?format=json", iso3Code)
+
+		resp, err := http.Get(
+			infoFormatString,
+		)
+
+		if err != nil {
+			err1 = fmt.Errorf("Failed to send request: %v", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			err1 = fmt.Errorf("Failed to read response: %v", err)
+			return
+		}
+
+		var response []json.RawMessage
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			err1 = fmt.Errorf("Failed to decode JSON envelope: %v", err)
+			return
+		}
+
+		err = json.Unmarshal(response[1], &infoData)
+		if err != nil {
+			err1 = fmt.Errorf("Failed to decode infoData: %v", err)
+			return
+		}
+
+	}()
+
+
+	go func() {
+		defer wg.Done()
+
+		gdpFormatString := fmt.Sprintf("https://api.worldbank.org/v2/country/%s/indicator/NY.GDP.MKTP.CD?date=2025&format=json", iso3Code)
+
+		resp, err := http.Get(
+			gdpFormatString,
+		)
+
+		if err != nil {
+			err2 = fmt.Errorf("GDP request failed: %w", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			err2 = fmt.Errorf("failed reading GDP body: %w", err)
+			return
+		}
+
+		var response []json.RawMessage
+		err = json.Unmarshal(body, &response)
+		if err != nil {
+			err2 = fmt.Errorf("Failed to decode JSON envelope: %v", err)
+			return
+		}
+
+		err = json.Unmarshal(response[1], &gdpData)
+		if err != nil {
+			err2 = fmt.Errorf("Failed to decode gdpData: %v", err)
+			return
+		}
+
+
+
+	}()
+
+	wg.Wait()
+
+	// Check if either Goroutine captured an error
+	if err1 != nil {
+		return country.Country{}, err1
 	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Failed to read response: %v", err)
+	if err2 != nil {
+		return country.Country{}, err2
 	}
 
-	var response []json.RawMessage
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		log.Fatalf("Failed to decode JSON envelope: %v", err)
+	// Validate non-empty responses
+	if len(infoData) == 0 || len(gdpData) == 0 {
+		return country.Country{}, fmt.Errorf("empty payload returned for ISO3: %s", iso3Code)
 	}
 
-	// Unmarshal element [1] into a slice of CountryInfo
-	var countries []worldbank.CountryInfo
-	err = json.Unmarshal(response[1], &countries)
-	if err != nil {
-		log.Fatalf("Failed to decode country array: %v", err)
-	}
-
-	// Iterate and print the ISO3 code (stored in .ID)
-	for _, c := range countries {
-		fmt.Printf("ISO3 (ID): %-5s | ISO2: %-4s | Name: %s\n", c.ID, c.ISO2Code, c.Name)
-	}
+	// Safely dereference GDP pointer if handling *float64
+	var finalGDP float64
+	finalGDP = gdpData[0].GDPValue
+	
+	// Assemble final combined Country struct
+	return country.Country{
+		CountryID: infoData[0].CountryID,
+		ISO3Code:  iso3Code,
+		Name:      infoData[0].Name,
+		GDP:       finalGDP,
+	}, nil	
 }
 
 
@@ -216,10 +305,76 @@ func requestCountryByISO3(iso3Code string) country.Country {
 
 }
 
+// add autocomplete if the user mispells the country there will be a key error
+func getISO3Identifier(countryName string) (string) { //countryName string
+	iso3, ok := countriesMap[countryName]
+	if !ok {
+		return "" //adjust this later
+	}
+	return iso3
+}
+
+//Primary method of fetching a country
+func requestCountryByName(countryName string) country.Country {
+
+	// Use get ISO3 identifier to get the country, returns ticker like USA
+	iso3String := getISO3Identifier(countryName)
+
+	// We now have a country struct
+	country := requestCountryByISO3(iso3String)
+
+
+	//println(country.Name)
+	//fmt.Printf("%.0f\n", country.GDP)
+
+
+	return country
+}
+
+func initData() {
+
+	resp, err := http.Get("http://api.worldbank.org/v2/country?format=json&per_page=500")
+	if err != nil {
+		log.Fatalf("Failed to send request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response: %v", err)
+	}
+
+	var response []json.RawMessage
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		log.Fatalf("Failed to decode JSON envelope: %v", err)
+	}
+
+	// Unmarshal element [1] into a slice of CountryInfo
+	var countries []worldbank.CountryInfo
+	err = json.Unmarshal(response[1], &countries)
+	if err != nil {
+		log.Fatalf("Failed to decode country array: %v", err)
+	}
+
+	for _, country := range countries {
+		//fmt.Printf("ISO3 (ID): %-5s | ISO2: %-4s | Name: %s\n", country.ID, country.ISO2Code, country.Name)
+
+		//countriesMap[country.ID] = country.Name
+		countriesMap[country.Name] = country.ID
+
+	}
+	//fmt.Println(countriesMap)
+
+	
+	
+}
+
 func initServer() {
 	fmt.Println("Server Started!!")
-	var aCountry = requestCountryByISO3("USA")
-	println(aCountry.Name)
-	fmt.Printf("%.0f\n", aCountry.GDP)
-	//makeRequest()
+	//add all values to country map
+	initData()
+	country := requestCountryByName("Mexico")
+	fmt.Println(country.Name)
+	fmt.Printf("%.0f\n", country.GDP)
 }
